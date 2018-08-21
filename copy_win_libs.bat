296e8d3f649b7528ccad0968073a9fd3955b2819
Jeffery Myers <JeffM2501@gmail.com>
2018-08-21 11:31:16 -0700

    just include openTK directly. it's simpler.

--- copy_win_libs.bat
+++ copy_win_libs.bat
@@ -1,7 +1,16 @@
 if exist _bin_debug\ (
-echo Copying to debug
+echo Copying Native Libs to debug
 xcopy /A /R /E Libs\msvc\DLLx86\*.dll _bin_debug )
+
+if exist _bin_debug\ (
+echo Copying OpenTK to debug
+xcopy /A /R /E Libs\OpenTK\Lib\*.* _bin_debug )
+
 
 if exist _bin_release\ (
 echo Copying to release
 xcopy /A /R /E Libs\msvc\DLLx86\*.dll _bin_release )
+
+if exist _bin_release\ (
+echo Copying OpenTK to release
+xcopy /A /R /E Libs\OpenTK\Lib\*.* _bin_release )
