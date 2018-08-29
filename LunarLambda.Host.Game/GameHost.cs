4becb18e512af776851abbfc736253fd914fa195
Jeffery Myers <JeffM2501@gmail.com>
2018-08-29 16:04:39 -0700

    add game host, ship client, and shared message module.

---
+++ LunarLambda.Host.Game/GameHost.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+using LunarLambda.API;
+
+namespace LunarLambda.Host.Game
+{
+    public class GameHost
+    {
+        public LLScenario ActiveScenario = null;
+
+        public void Startup()
+        {
+
+        }
+    }
+}
