ef6a6a0012eb43299d13db90c5a5e55b16879dc2
Jeffery Myers <JeffM2501@gmail.com>
2018-08-30 09:35:20 -0700

    stub out testing framework for EE Lua compatibility plugin.

---
+++ Plugins/EELuaCompat/EELua/EECompatibility.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LunarLambda.API;
+
+namespace EELua
+{
+    public class EECompatibility : LLPlugin
+    {
+        public override void Load()
+        {
+            // scan scripts and register scenarios
+        }
+
+        public override void Shutdown()
+        {
+            // clean up lua
+        }
+
+        public override void Startup()
+        {
+            // warm up scripting
+        }
+
+        public override void Unload()
+        {
+            // probably not needed
+        }
+    }
+}
---
+++ Plugins/EELuaCompat/EELuaEval/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MoonSharp.Interpreter;
+using MoonSharp.Interpreter.Loaders;
+
+namespace EELuaEval
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Script.WarmUp();
+
+            try
+            {
+                Script script = new Script();
+
+                ((ScriptLoaderBase)script.Options.ScriptLoader).ModulePaths = new string[] { "E:\\Projects\\EmptyEpsilon\\EmptyEpsilon\\scripts\\" };
+                script.DoFile("test.lua");
+
+                DynValue res = script.Call(script.Globals["init"]);
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
