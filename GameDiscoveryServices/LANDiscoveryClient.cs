a679afa5a76080a131c336705930c4f00c949106
Jeffery Myers <JeffM2501@gmail.com>
2018-09-04 16:51:03 -0700

    LAN Discovery services.

---
+++ GameDiscoveryServices/LANDiscoveryClient.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace GameDiscoveryServices
+{
+    public static class LANDiscoveryClient
+    {
+        public class LANHost
+        {
+            public IPAddress LANAddress = null;
+            public TcpClient Client = null;
+        }
+
+        private static List<LANHost> ActiveScanHosts = new List<LANHost>();
+        private static List<IPAddress> PendingAddresses = new List<IPAddress>();
+    }
+}
---
+++ GameDiscoveryServices/LanDiscoveryHost.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace GameDiscoveryServices
+{
+    public static class LANDiscoveryHost
+    {
+        private static readonly int DiscoveryPort = 1700;
+
+        private static TcpListener SocketHost = null;
+
+        public static void Startup()
+        {
+            if (SocketHost != null)
+                return;
+
+            SocketHost = new TcpListener(IPAddress.Any, DiscoveryPort);
+
+            SocketHost.Start();
+            SocketHost.BeginAcceptTcpClient(AcceptRequest, null);
+        }
+
+        public static void Shutdown()
+        {
+            if (SocketHost != null)
+            {
+                SocketHost.Stop();
+            }
+
+            SocketHost = null;
+        }
+
+        private static void AcceptRequest(IAsyncResult result)
+        {
+            var client = SocketHost.EndAcceptTcpClient(result);
+            SocketHost.BeginAcceptTcpClient(AcceptRequest, null);
+
+            if (ServiceList.LocalService == null)
+                client.Close();
+            else
+            {
+                DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(HostedService));
+                serializer.WriteObject(client.GetStream(), ServiceList.LocalService);
+                client.GetStream().Flush();
+                client.Close();
+            }
+        }
+    }
+}
