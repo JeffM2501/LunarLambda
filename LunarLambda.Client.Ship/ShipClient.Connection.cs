1a57ab9591c24385b3d18bd0fa156cde3193b468
Jeffery Myers <JeffM2501@gmail.com>
2018-09-08 08:44:59 -0700

    add ship connector

---
+++ LunarLambda.Client.Ship/ShipClient.Connection.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using LunarLambda.Messges.Ship;
+using LunarLambda.Messges.Ship.Connect;
+using LunarLambda.Messges.Ship.Game;
+
+namespace LunarLambda.Client.Ship
+{
+    public partial class ShipClient
+    {
+        public event EventHandler<ConnectResponce> ConnectionRejected = null;
+        public event EventHandler<ConnectResponce> ConnectionAccepted = null;
+
+        public event EventHandler<UpdateShipList> ShipListUpdated = null;
+
+        public ConnectResponce LastConnectResponce { get; protected set; } = null;
+        public UpdateShipList LastShipList { get; protected set; } = null;
+
+        public void HandleConnectResponce(object sender, ShipMessage message)
+        {
+            LastConnectResponce = message as ConnectResponce;
+
+            if (LastConnectResponce.Responce == ConnectResponce.ResponceTypes.Rejected)
+                ConnectionRejected?.Invoke(this, LastConnectResponce);
+            else
+                ConnectionAccepted?.Invoke(this, LastConnectResponce);
+        }
+
+        public void HandleUpdateShipList (object sender, ShipMessage message)
+        {
+            LastShipList = message as UpdateShipList;
+            ShipListUpdated?.Invoke(this, LastShipList);
+        }
+    }
+}
